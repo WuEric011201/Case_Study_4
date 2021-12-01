7e53400c1afda3d8d25e39dcf56edb9fb86699ce
Thomas Lang <thomaslang2003@me.com>
2021-12-01 14:59:53 -0600

    q2 pt1

---
+++ lens.m
@@ -0,0 +1,36 @@
+%% File to simulate rays hitting lense
+
+d1 = 0.2;
+M1 = [1, d1, 0, 0; 0, 1, 0, 0; 0, 0, 1, d1; 0, 0, 0, 1];
+
+f = 0.1;
+M2 = [1, 0, 0, 0; -1/f, 1, 0, 0; 0, 0, 1, 0; 0, 0, -1/f, 1];
+
+d3 = 0.3;
+M3 = [1, d3, 0, 0; 0, 1, 0, 0; 0, 0, 1, d3; 0, 0, 0, 1];
+
+M = M3*M2*M1;
+
+% Setup rays for all position 0 ones
+ray1 = [0; pi/20; 0; 0];
+ray2 = [0; pi/40; 0; 0];
+ray3 = [0; 0; 0; 0];
+ray4 = [0; -pi/40; 0; 0];
+ray5 = [0; -pi/20; 0; 0];
+
+% Rays for 10mm
+ray6 = [10*10^-3; pi/20; 0; 0];
+ray7 = [10*10^-3; pi/40; 0; 0];
+ray8 = [10*10^-3; 0; 0; 0];
+ray9 = [10*10^-3; -pi/40; 0; 0];
+rayA = [10*10^-3; -pi/20; 0; 0];
+
+allRays = [ray1, ray2, ray3, ray4, ray5, ray6, ray7, ray8, ray9, rayA];
+
+raysOpen = M1*allRays;
+
+raysLense = M2*raysOpen;
+
+raysConverge = M3*raysLense;
+
+plot([0, d1, d1+d3], [allRays(1, :)', raysOpen(1, :)', raysConverge(1, :)']);
--- rayPropogate.m
+++ rayPropogate.m
@@ -1,37 +1,23 @@
 close all
 %% File to simulate rays propogating in xz plane
 
-d = 0.02;
-
-% % Setup rays for all position 0 ones
-% ray1 = [[0; pi/20], zeros(2, 1)];
-% ray2 = [[0; pi/10], zeros(2, 1)];
-% ray3 = [[0; 0], zeros(2, 1)];
-% ray4 = [[0; -pi/10], zeros(2, 1)];
-% ray5 = [[0; -pi/20], zeros(2, 1)];
-% 
-% % Rays for 10mm
-% ray6 = [[10*10^-3; pi/20], zeros(2, 1)];
-% ray7 = [[10*10^-3; pi/10], zeros(2, 1)];
-% ray8 = [[10*10^-3; 0], zeros(2, 1)];
-% ray9 = [[10*10^-3; -pi/10], zeros(2, 1)];
-% rayA = [[10*10^-3; -pi/20], zeros(2, 1)];
+d = 0.2;
 
 % Setup rays for all position 0 ones
-ray1 = [0; pi/20];
-ray2 = [0; pi/10];
-ray3 = [0; 0];
-ray4 = [0; -pi/10];
-ray5 = [0; -pi/20];
+ray1 = [0; pi/20; 0; 0];
+ray2 = [0; pi/40; 0; 0];
+ray3 = [0; 0; 0; 0];
+ray4 = [0; -pi/20; 0; 0];
+ray5 = [0; -pi/40; 0; 0];
 
 % Rays for 10mm
-ray6 = [10*10^-3; pi/20];
-ray7 = [10*10^-3; pi/10];
-ray8 = [10*10^-3; 0];
-ray9 = [10*10^-3; -pi/10];
-rayA = [10*10^-3; -pi/20];
+ray6 = [10*10^-3; pi/20; 0; 0];
+ray7 = [10*10^-3; pi/40; 0; 0];
+ray8 = [10*10^-3; 0; 0; 0];
+ray9 = [10*10^-3; -pi/40; 0; 0];
+rayA = [10*10^-3; -pi/20; 0; 0];
 
-Md = [1, d; 0, 1];
+Md = [1, d, 0, 0; 0, 1, 0, 0; 0, 0, 1, d; 0, 0, 0, 1];
 
 allRays = [ray1, ray2, ray3, ray4, ray5, ray6, ray7, ray8, ray9, rayA];
 
